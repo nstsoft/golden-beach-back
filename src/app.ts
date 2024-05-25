1c368d3ef7bba892eded12d12ee464e548b6965e
Oleg <keeperoleg26@gmail.com>
2024-05-25 10:50:52 +0300

    feat(middlewares)

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import {} from 'http-errors';
 
 import { userService } from './constructors';
 import { UserController } from './controllers';
@@ -11,8 +12,14 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1', new UserController(userService).route);
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.use(function clientErrorHandler(err: any, req: Request, res: Response, _: NextFunction) {
+  console.log(err);
+  return res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 export { app };
